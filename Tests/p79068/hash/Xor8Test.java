17eec6f70930c2224abe6f4acc61356f6716ac4f
Nayuki Minase <nayuki@eigenstate.org>
2011-03-27 03:57:53 +0000

    Xor8Test: Updated test cases.

--- Tests/p79068/hash/Xor8Test.java
+++ Tests/p79068/hash/Xor8Test.java
@@ -8,14 +8,15 @@ public final class Xor8Test {
 	@Test
 	public void testXor8() {
 		HashUtils.testAscii(Xor8.FUNCTION, "", "00");
-		HashUtils.testAscii(Xor8.FUNCTION, "bb", "00");
+		HashUtils.testAscii(Xor8.FUNCTION, "A", "41");
+		HashUtils.testAscii(Xor8.FUNCTION, "AA", "00");
 		HashUtils.testAscii(Xor8.FUNCTION, "asdf", "10");
-		HashUtils.testAscii(Xor8.FUNCTION, "fads", "10");
+		HashUtils.testAscii(Xor8.FUNCTION, "fsda", "10");
 		HashUtils.testAscii(Xor8.FUNCTION, "The", "59");
 		HashUtils.testAscii(Xor8.FUNCTION, "tEh", "59");
-		HashUtils.testAscii(Xor8.FUNCTION, "soy", "65");
-		HashUtils.testAscii(Xor8.FUNCTION, "sorry", "65");
-		HashUtils.testAscii(Xor8.FUNCTION, "sorrrry", "65");
+		HashUtils.testAscii(Xor8.FUNCTION, "ac", "02");
+		HashUtils.testAscii(Xor8.FUNCTION, "abbc", "02");
+		HashUtils.testAscii(Xor8.FUNCTION, "abbbbc", "02");
 	}
 	
 }
