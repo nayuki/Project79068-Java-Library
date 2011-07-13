acf6139baba7d38abd5c7623cdc5bfd3bf0289fd
Nayuki Minase <nayuki@eigenstate.org>
2011-07-13 04:19:51 +0000

    Crc: Renamed variables, tweaked code.

--- Source/p79068/hash/Crc.java
+++ Source/p79068/hash/Crc.java
@@ -16,10 +16,10 @@ public final class Crc extends AbstractHashFunction {
 	 *  <li>{@code hashLength = 4}</li>
 	 *  <li>{@code degree = 32}</li>
 	 *  <li>{@code polynomial = 0x104C11DB7L} (<var>x</var><sup>32</sup> + <var>x</var><sup>26</sup> + <var>x</var><sup>23</sup> + <var>x</var><sup>22</sup> + <var>x</var><sup>16</sup> + <var>x</var><sup>12</sup> + <var>x</var><sup>11</sup> + <var>x</var><sup>10</sup> + <var>x</var><sup>8</sup> + <var>x</var><sup>7</sup> + <var>x</var><sup>5</sup> + <var>x</var><sup>4</sup> + <var>x</var><sup>2</sup> + <var>x</var><sup>1</sup> + <var>x</var><sup>0</sup>)</li>
-	 *  <li>{@code reversein = true}</li>
-	 *  <li>{@code reverseout = true}</li>
-	 *  <li>{@code xorin = 0xFFFFFFFFL}</li>
-	 *  <li>{@code xorout = 0xFFFFFFFFL}</li>
+	 *  <li>{@code reverseIn = true}</li>
+	 *  <li>{@code reverseOut = true}</li>
+	 *  <li>{@code xorIn = 0xFFFFFFFFL}</li>
+	 *  <li>{@code xorOut = 0xFFFFFFFFL}</li>
 	 * </ul>
 	 */
 	public static final Crc CRC32_FUNCTION = new Crc("CRC-32", 32, 0x104C11DB7L, true, true, 0xFFFFFFFFL, 0xFFFFFFFFL);
@@ -40,12 +40,12 @@ public final class Crc extends AbstractHashFunction {
 	 * @param name the name of the hash function
 	 * @param deg the degree of the polynomial used
 	 * @param poly the polynomial
-	 * @param revin whether the bits in each input byte will be reversed
-	 * @param revout whether the bits in the output hash value will be reversed
-	 * @param xorin the value to XOR with the first degree bits of the input (after input reversal)
-	 * @param xorout the value to XOR with the output hash value (before output reversal)
+	 * @param revIn whether the bits in each input byte will be reversed
+	 * @param revOut whether the bits in the output hash value will be reversed
+	 * @param xorIn the value to XOR with the first degree bits of the input (after input reversal)
+	 * @param xorOut the value to XOR with the output hash value (before output reversal)
 	 */
-	public Crc(String name, int deg, long poly, boolean revin, boolean revout, long xorin, long xorout) {
+	public Crc(String name, int deg, long poly, boolean revIn, boolean revOut, long xorIn, long xorOut) {
 		super(name, (deg + 7) / 8);
 		if (name == null)
 			throw new IllegalArgumentException();
@@ -54,18 +54,18 @@ public final class Crc extends AbstractHashFunction {
 		if (deg < 64) {
 			if ((poly >>> deg) != 1)
 				throw new IllegalArgumentException("Illegal polynomial");
-			if ((xorin >>> deg) != 0)
+			if ((xorIn >>> deg) != 0)
 				throw new IllegalArgumentException("Invalid initial XOR constant");
-			if ((xorout >>> deg) != 0)
+			if ((xorOut >>> deg) != 0)
 				throw new IllegalArgumentException("Invalid final XOR constant");
 		}
 		this.name = name;
 		this.degree = deg;
 		this.polynomial = poly;
-		this.reverseInputBits = revin;
-		this.reverseOutputBits = revout;
-		this.xorInput = xorin;
-		this.xorOutput = xorout;
+		this.reverseInputBits = revIn;
+		this.reverseOutputBits = revOut;
+		this.xorInput = xorIn;
+		this.xorOutput = xorOut;
 	}
 	
 	
@@ -89,12 +89,20 @@ public final class Crc extends AbstractHashFunction {
 	 */
 	@Override
 	public boolean equals(Object obj) {
-		if (super.equals(obj))
+		if (obj == this)
 			return true;
-		if (!(obj instanceof Crc))
+		else if (!(obj instanceof Crc))
 			return false;
-		Crc hashfunc = (Crc)obj;
-		return name.equals(hashfunc.name) && degree == hashfunc.degree && polynomial == hashfunc.polynomial && reverseInputBits == hashfunc.reverseInputBits && reverseOutputBits == hashfunc.reverseOutputBits && xorInput == hashfunc.xorInput && xorOutput == hashfunc.xorOutput;
+		else {
+			Crc other = (Crc)obj;
+			return    name.equals(other.name)
+			       && degree == other.degree
+			       && polynomial == other.polynomial
+			       && reverseInputBits == other.reverseInputBits
+			       && reverseOutputBits == other.reverseOutputBits
+			       && xorInput == other.xorInput
+			       && xorOutput == other.xorOutput;
+		}
 	}
 	
 	
