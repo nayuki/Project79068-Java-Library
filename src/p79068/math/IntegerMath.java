d143d2e817eab91896772662e6f571bfe2ec4b7a
Nayuki Minase <nayuki@eigenstate.org>
2013-01-19 20:52:09 +0000

    IntegerMath: Optimized sqrt() with less expensive arithmetic operations.

--- src/p79068/math/IntegerMath.java
+++ src/p79068/math/IntegerMath.java
@@ -223,10 +223,10 @@ public final class IntegerMath {
 		if (x < 0)
 			throw new IllegalArgumentException("Square root of negative number");
 		int y = 0;
-		for (int i = 32768; i >= 1; i /= 2) {
-			y += i;
+		for (int i = 1 << 15; i != 0; i >>>= 1) {
+			y |= i;
 			if (y > 46340 || y * y > x)
-				y -= i;
+				y ^= i;
 		}
 		return y;
 	}
